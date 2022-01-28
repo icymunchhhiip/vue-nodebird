9ef5d244a9f313dbba0d68b01253d17233eb49e3
icymunchhhiip <fsec1sun@gmail.com>
2022-01-28 14:11:18 +0900

    :art: Improve Structure

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ npm init
 ```
 npm i vue nuxt
 ```
+
+# What is Nuxt ?
+* vue 기반 웹사이트를 위한 프레임워크
+* 컴포넌트보다는 page 중심 (폴더 이름을 꼭 `pages`라고 써야함!!)
+* pages 안을 자동으로 페이지로 만들어줌
+
+# Run
+```
+npm run dev
+```
---
+++ ch1/front/pages/signup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    sign up page
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      name: 'Nuxt.js',
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
